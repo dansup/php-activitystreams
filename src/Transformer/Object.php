5daef9013664a052abe393d45b23e80d27fc5d15
Daniel Supernault <danielsupernault@gmail.com>
2017-04-20 19:38:07 -0600

    Update object transformer

--- src/Transformer/Object.php
+++ src/Transformer/Object.php
@@ -6,15 +6,47 @@ use Dansup\ActivityStreams\Model\Object as ObjectType;
 
 class Object {
 
-  public static function basic(ObjectType $data)
+  protected $data;
+
+  public function __construct(ObjectType $data)
   {
-    return [
+    $this->data = $data;
+  }
+
+  public function render($template = 'basic')
+  {
+    switch ($template) {
+      case 'basic':
+          $res = 'basic';
+        break;
+      
+      default:
+          $res = 'basic';
+        break;
+    }
+    return $this->{$res}();
+  }
+
+  public function basic()
+  {
+    $data = $this->data;
+    $template = [
       '@context' => $data->getContext(),
       'type' => $data->getType(),
       'id' => $data->getId(),
       'name' => $data->getName(),
-      'content' => $data->getContent()
+      'attributedTo' => $data->getAttributedTo(),
+      'content' => $data->getContent(),
+      'published' => $data->published(),
     ];
+    if( count($data->addedFields()) > 0 ) {
+      foreach ($data->addedFields() as $field) {
+        $field = lcfirst($field);
+        $res = $data->{$field}();
+        $template[$field] = $res;
+      }
+    }
+    return $template;
   }
 
 }
